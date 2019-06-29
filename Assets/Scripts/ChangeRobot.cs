2a805abc32b23538eb21f998efafe6a653a6d40e
pinct <51133746+pinct@users.noreply.github.com>
2019-06-28 20:30:41 -0400

    Initial commit

---
+++ Assets/Scripts/ChangeRobot.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets._2D;
+using Cinemachine;
+
+public class ChangeRobot : MonoBehaviour
+{
+	public GameObject robot;
+	public GameObject currbot;
+	public GameObject startbot;
+	public GameObject VCAM;
+    //Start is called before the first frame update
+    void Start()
+    {
+        if (robot.name != "StartingCharacter")
+		{
+			robot.GetComponent<PlatformerCharacter2D>().enabled = false;
+			robot.GetComponent<Platformer2DUserControl>().enabled = false;
+		}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	void OnMouseDown()
+	{
+		currbot.GetComponent<PlatformerCharacter2D>().enabled = false;
+		currbot.GetComponent<Platformer2DUserControl>().enabled = false;
+		robot.GetComponent<PlatformerCharacter2D>().enabled = true;
+		robot.GetComponent<Platformer2DUserControl>().enabled = true;
+		VCAM.GetComponent<CinemachineVirtualCamera>().Follow = robot.transform;
+		startbot.GetComponent<ChangeRobot>().currbot = robot;
+		
+	}
+}
